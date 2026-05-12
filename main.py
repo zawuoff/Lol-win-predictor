1beb0e127376e66eefd119232f801d3927a6190f
zawuoff <fouwaz232@gmail.com>
2026-05-12 23:53:09 +0100

    switching the project to be a build shower for now, ininital commit just getting the summoners name from the endpoint

--- main.py
+++ main.py
@@ -2,27 +2,22 @@ from html5lib.treewalkers import pprint
 import requests, pprint
 
 riot_url = "https://127.0.0.1:2999/liveclientdata/allgamedata"
+response = requests.get(riot_url, verify=False)
+response_json = response.json()
+
+active_summoner_name = response_json["activePlayer"]["summonerName"]
+
+print(active_summoner_name)
 
 
 
-
-try: 
-    response = requests.get(riot_url, verify=False)
-    response_json = response.json()
-    # print(response_json.keys()) 
-    # print(response_json["allPlayers"])
-    # print("connected to live league api")
-    pprint.pprint(response_json["allPlayers"][0])
-    """
-    for players in response_json["allPlayers"]:
-        if players["team"] == "ORDER":
-            players["team"] = "BLUE"
-        if players["team"] == "CHAOS":
-            players["team"] = "RED"
-        print(f"{players["championName"]} - {players["team"]}")
-    """
-except:
-    print("No Active League Data")
+# try: 
+#     response = requests.get(riot_url, verify=False)
+#     response_json = response.json()
+#     pprint.pprint(response_json["allPlayers"])
+    
+# except:
+#     print("No Active League Data")
 
 
 # print(response.status_code)
