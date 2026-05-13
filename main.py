96fcdce069d32e880f0fda63c7816e8cbc56c346
zawuoff <fouwaz232@gmail.com>
2026-05-13 01:25:21 +0100

    added more builds to the build.json file, i think it should have everytthing for now, basically scrapped everything from opgg and created that just for now, later will do this using an API  also then made a way to find the build for the champion you are playing actively

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from html5lib.treewalkers import pprint
-import requests, pprint
+import requests, pprint, json, os
 
 riot_url = "https://127.0.0.1:2999/liveclientdata/allgamedata"
 response = requests.get(riot_url, verify=False)
 response_json = response.json()
-
 active_summoner_name = response_json["activePlayer"]["summonerName"]
-
+current_dir = os.path.dirname(__file__)
+file_path = os.path.join(current_dir, "builds.json")
+print(file_path)
 print(active_summoner_name)
 
 def get_summoner_champion(summoner_name):
@@ -15,16 +16,10 @@ def get_summoner_champion(summoner_name):
         if players["summonerName"] == summoner_name:
             champion_name = players["championName"]
     return champion_name
-        
 
-print(get_summoner_champion(active_summoner_name))
-# try: 
-#     response = requests.get(riot_url, verify=False)
-#     response_json = response.json()
-#     pprint.pprint(response_json["allPlayers"])
-    
-# except:
-#     print("No Active League Data")
+champion_name = get_summoner_champion(active_summoner_name)
 
-
-# print(response.status_code)
+with open(file_path, 'r') as builds:
+    build_data = json.load(builds)
+    if champion_name in build_data:
+        print(build_data[champion_name]["core_build"])
